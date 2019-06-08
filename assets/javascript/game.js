9310cb364de10c22c75de95a2c82f6580683034d
Bryon <bryonlh@gmail.com>
2019-06-07 20:13:32 -0700

    updated index tags and added variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,7 @@
+//Variables here
+
 var wins = 0;
 var losses = 0;
+var maxGuesses = 9;
+var availableLetters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+
