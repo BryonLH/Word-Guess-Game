d8bbd1515461cdf2eb87c7fe4de3c20d1a6bb7dc
Bryon <bryonlh@gmail.com>
2019-06-07 20:15:44 -0700

    added word list variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,4 +4,4 @@ var wins = 0;
 var losses = 0;
 var maxGuesses = 9;
 var availableLetters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
-
+var wordList = ["jarjar", "palpatine"];
