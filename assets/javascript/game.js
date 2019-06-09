c6179f4bec63b2b6078237e817459553682dc12a
BryonLH <bryonlh@gmail.com>
2019-06-08 17:55:51 -0700

    added randomize word var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,4 +4,11 @@ var wins = 0;
 var losses = 0;
 var maxGuesses = 9;
 var availableLetters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
-var wordList = ["jarjar", "palpatine"];
+var wordList = ["jarjar", "farfar"];
+
+var word = wordList[Math.floor(Math.random() * wordList.length)];
+console.log(word);
+
+
+
+
