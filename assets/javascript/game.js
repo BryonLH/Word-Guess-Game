5579f4135223420cff1b5d007c1ffed3fd0f9503
Bryon Harland <bryonlh@gmail.com>
2019-11-01 21:30:34 -0700

    updatewd functioning app

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,94 +1,88 @@
-//Variables here
-
-
-function getAllIndices(arr, val) {
-    var indices = [], i = -1;
-    while ((i = arr.indexOf(val, i + 1)) != -1) {
-        indices.push(i);
-    }
-    return indices;
-}
-
-// var word = wordList[Math.floor(Math.random() * wordList.length)];
-// console.log(word);
-// document.getElementById("word-display-letters").innerText = word;
-
+// Variables here
 var wins = 0;
 var losses = 0;
 var maxGuesses = 9;
 var userGuesses = [];
 var availableLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var underScoreArray = ["_", "_", "_", "_", "_", "_"];
+var blanks = 0;
+var letterArray = [];
+var wordAfterGuess = [];
 var wordList = ["rampage", "unbreakable", "armageddon"];
-// var wordArray = word.split('');
-// var indicies = getAllIndices(wordArray, 'm')
-// indicies.forEach(function(i) {
-//     underScorwArray[i] = wordArray[i]
-// })
-// console.log(wordArray);
-// console.log(underScoreArray);
-var anyKeyToStart = ["press any key to start"];
-var anyKeyToReset = ["press any key to reset"];
+var word = '';
 
-var youWin = ["you win!"];
-var youLose = ["you lose!"];
-var gameOver = false;
 
-for(var i = 0; i < wordList.length; i++) {
+for (var i = 0; i < wordList.length; i++) {
     console.log(wordList[i]);
 }
 
-//Randomize word here
-var word = wordList[Math.floor(Math.random() * wordList.length)];
-
-var getRandomWord = function () {
-    return wordList[Math.floor(Math.random() * wordList.length)];
-};
-
-// var indexes = getAllIndices(wordArray)
-// indexes.forEach(function(i) {
-//     underScoreArray[i] = wordArray[i]
-// })
-// console.log(wordArray)
-// var wordArray = word.split("");
-
-// function for onkeyup
-// document.onkeyup = function(event) {
-//     console.log(event.key);
-// }
-
-// enter wins++ to add to wins
-
-// function for when user clicks a key
-document.onkeyup = function(event) {
-    var userGuess = event.key;
-    console.log(event.key);
-    userGuesses.splice(1, 0, event.key);
-    //  if(userGuess === )
-    document.getElementById("guessed-letters").textContent = userGuesses;
+function checkGuess(letter) {
+    var letterExists = false;
+    for (var i = 0; i < blanks; i++) {
+        if (word[i] === letter) {
+            letterExists = true;
+        }
+    }
+    if (letterExists) {
+        for (var j = 0; j < blanks; j++) {
+            if (word[j] === letter) {
+                wordAfterGuess[j] = letter;
+            }
+        }
+        console.log(wordAfterGuess);
+    } else {
+        userGuesses.push(letter);
+        maxGuesses--;
+    }
 }
 
-
+// This is the function to run when the user clicks the button to start a new game
 function newGame() {
     gameOver = false;
     console.log(gameOver);
-    getRandomWord();
+    word = wordList[Math.floor(Math.random() * wordList.length)];
     console.log(word);
-    var wordArray = word.split("");
-    console.log(wordArray);
-    document.getElementById("word-display").innerHTML = wordArray;
+    var letterArray = word.split("");
+    console.log(letterArray);
+    blanks = letterArray.length;
+    console.log("blanks = " + blanks);
+    wordAfterGuess = [];
+    userGuesses = [];
+    for (var i = 0; i < blanks; i++) {
+        wordAfterGuess.push("_");
+    }
+    console.log(wordAfterGuess);
+    // This section updates and displays he score and guesses remaining
+    document.getElementById("remaining-guesses").innerHTML = maxGuesses;
+    document.getElementById("word-display-letters").innerHTML = wordAfterGuess.join(" ");
+    document.getElementById("guessed-letters").innerHTML = userGuesses.join(" ");
 }
-console.log(userGuesses);
-// function userClicksLetter() {
-//     var x = document.getElementById("guessed-letters").value;
-//     userGuesses.push(onclick);
-//     document.getElementById("guessed-letters").innerHTML = x;
 
-//     console.log(userGuesses);
-//     console.log(x);
-// }
+function updateGame() {
+    // This section updates and displays he score and guesses remaining
+    document.getElementById("remaining-guesses").innerHTML = maxGuesses;
+    document.getElementById("word-display-letters").innerHTML = wordAfterGuess.join(" ");
+    document.getElementById("guessed-letters").innerHTML = userGuesses.join(" ");
+    // determine if the user wins or losses
+    if (letterArray.toString() === wordAfterGuess.toString()) {
+        wins++;
+        alert("You win!");
+        document.getElementById("win-count").innerHTML = wins;
+    }
+    else if (maxGuesses === 0) {
+        losses++;
+        alert("You lose");
+        document.getElementById("loss-count").innerHTML = losses;
+    }
+}
 
-
+// function for when user clicks a key
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    console.log(userGuess);
+    // userGuesses.splice(1, 0, event.key);
+    checkGuess(userGuess);
+    updateGame();
+}
 
 
 
